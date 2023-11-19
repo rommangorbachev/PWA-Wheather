c8c5d5602d305dc9ec4c1b7bd90e9e8d23be6694
roman.gorbachev <rn.gorbachev@gmail.com>
2023-11-19 15:34:45 +0600

    Added icons for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
 import {WeatherService} from "./services";
-import { WidgetWeatherComponent } from './components/widget-weather/widget-weather.component';
+import { WidgetWeatherComponent } from './components';
 import {MatIconModule} from "@angular/material/icon";
 import {MatCardModule} from "@angular/material/card";
 import {MatInputModule} from "@angular/material/input";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,11 @@ import {MatInputModule} from "@angular/material/input";
     HttpClientModule,
     MatIconModule,
     MatCardModule,
-    MatInputModule
+    MatInputModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [WeatherService],
   bootstrap: [AppComponent]
